5141c09a4ad0ca5647a61ecafe347f93dcd224e8
Adam Obrzut <obrzutadam2@gmail.com>
2025-10-28 23:11:18 +0100

    zadania6-9

---
+++ 02-TypesAndVariables/6-6.py
@@ -0,0 +1,14 @@
+###
+# A program to print numerical representations of characters.
+#
+print(f'a {ord('a')}')
+print(f'b {ord('b')}')
+print(f'b {ord('z')}')
+print(f'b {ord('A')}')
+print(f'b {ord('Z')}')
+print(f'b {ord('1')}')
+print(f'b {ord('=')}')
+print(f'b {ord('+')}')
+print(f'b {ord('€')}')
+
+#ord() - zamienia znak na jej cyfrowy odpowiednik
