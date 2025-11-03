802bd05bad6bea4136323e506e0892ce0a73d1b9
Adam Obrzut <obrzutadam2@gmail.com>
2025-11-03 12:53:50 +0100

    zadania temat 2

---
+++ 03-ControlStructures/2-2.py
@@ -0,0 +1,19 @@
+###
+# A program for checking clothing sizes
+# S: Small size, M: Medium size, L: Large size
+# XL: Extra large size or Incorrect symbol (if entered symbol
+# dirrerent than S, M, L, XL)
+#
+size = (input('Enter size symbol: ')).upper()
+
+if size == 'S':
+    print('S: Small size')
+elif size == 'M':
+    print('M : medium size')
+elif size == 'L':
+    print('L : large size')
+elif size == 'M':
+    print('XL : Extra large size')
+
+else:
+    print('inncorrect value')
